e160b5ecf7cc053a7a9db194e355c4f0221b4064
Winter <summerlilyfs@icloud.com>
2026-08-30 03:02:15 +0100

    Started work on the gun script

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+    public float bulletSpeed;
+    public float bulletDamage;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
