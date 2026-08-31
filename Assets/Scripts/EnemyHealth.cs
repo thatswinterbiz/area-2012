31aa3c36c36062f8d39e0973f60a499ebd2d4ea0
Winter <summerlilyfs@icloud.com>
2026-08-31 01:38:53 +0100

    Started making player health script

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class EnemyHealth : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
