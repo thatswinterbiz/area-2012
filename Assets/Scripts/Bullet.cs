2770e543f4e76632ea6fd23b7c79c0560460efbf
Winter <summerlilyfs@icloud.com>
2026-09-08 02:01:13 +0100

    Attempting to make progress on the shooting script

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float bulletSpeed = 10f;
+    public float bulletDamage = 1f;
+}
--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -6,28 +6,32 @@ public class PlayerShooting : MonoBehaviour
     [SerializeField] private float cooldown = 0.25f;
     private float cooldownTimer;
 
-    //public GameObject bulletPrefab;
-    //public Transform firePoint;
-    //public float bulletForce;
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float bulletForce;
+    InputAction shootAction;
+
+    private void Start()
+    {
+        
+    }
 
     private void Update()
     {
-        cooldownTimer += Time.deltaTime;
+        //cooldownTimer += Time.deltaTime;
+
+        if(shootAction.IsPressed())
+        {
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation, null);
+        }
     }
 
-    private void Shoot()
-    {
-        if (cooldownTimer < cooldown) return;
+   // public void Shoot(InputAction.CallbackContext context)
+    //{
+       // if (cooldownTimer < cooldown) return;
 
-        Debug.Log("Shot!");
-        cooldownTimer = 0;
+       // Debug.Log("Shot!");
+       // cooldownTimer = 0;
        // GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation, null);
-    }
-
-    #region Input
-    private void OnShoot()
-    {
-        Shoot();
-    }
-    #endregion
+   // }
 }
