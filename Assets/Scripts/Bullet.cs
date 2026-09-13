4cd3ca5c2f7c2b57261eda2dd848c03b64e178a5
Winter <summerlilyfs@icloud.com>
2026-09-14 00:22:25 +0100

    Made the bullet actually shoot now

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,4 +4,11 @@ public class Bullet : MonoBehaviour
 {
     public float bulletSpeed = 10f;
     public float bulletDamage = 1f;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.linearVelocity = transform.right * bulletSpeed;
+    }
 }
--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -3,12 +3,8 @@ using UnityEngine.InputSystem;
 
 public class PlayerShooting : MonoBehaviour
 {
-    [SerializeField] private float cooldown = 0.25f;
-    private float cooldownTimer;
-
     public GameObject bulletPrefab;
     public Transform firePoint;
-    //InputAction shootAction;
 
     private void Start()
     {
@@ -19,23 +15,7 @@ public class PlayerShooting : MonoBehaviour
     {
         if(Mouse.current.leftButton.wasPressedThisFrame)
         {
-
+            Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         }
-
-        //cooldownTimer += Time.deltaTime;
-
-       // if(shootAction.IsPressed())
-        //{
-            //GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation, null);
-       // }
     }
-
-   // public void Shoot(InputAction.CallbackContext context)
-    //{
-       // if (cooldownTimer < cooldown) return;
-
-       // Debug.Log("Shot!");
-       // cooldownTimer = 0;
-       // GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation, null);
-   // }
 }
