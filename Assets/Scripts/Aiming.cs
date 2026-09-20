3f1ebca509a89d0b851b935b94b39f471bb36442
Winter <summerlilyfs@icloud.com>
2026-09-20 15:00:28 +0100

    Working through aiming script, about to make it work with the new input system

--- Assets/Scripts/Aiming.cs
+++ Assets/Scripts/Aiming.cs
@@ -1,6 +1,21 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Aiming : MonoBehaviour
 {
+    private Camera mainCam;
+    private Vector3 mousePos;
 
+    private void Start()
+    {
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 rotation = mousePos - transform.position;
+        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rotZ);
+    }
 }
