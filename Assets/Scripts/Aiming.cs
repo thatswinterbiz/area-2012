a73c25afeccbb26ba4b5485b95457eee18a28b44
Winter <summerlilyfs@icloud.com>
2026-09-21 18:38:15 +0100

    Decided to change player and gun sprites

--- Assets/Scripts/Aiming.cs
+++ Assets/Scripts/Aiming.cs
@@ -13,7 +13,7 @@ public class Aiming : MonoBehaviour
 
     private void Update()
     {
-        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        mousePos = mainCam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         Vector3 rotation = mousePos - transform.position;
         float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, rotZ);
